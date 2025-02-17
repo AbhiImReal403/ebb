8c7a94527e49debd96c173c39cb0c911c6aef13d
Abhilash <abhilashanil40404@gmail.com>
2025-02-17 21:54:19 +0530

    enhancement for avoiding relative videos

--- pages/about.vue
+++ pages/about.vue
@@ -12,7 +12,7 @@
     <iframe
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/VrhNVepG1lI"
+      src="https://www.youtube.com/embed/VrhNVepG1lI?rel=0"
       frameborder="0"
       allow="autoplay; encrypted-media"
       allowfullscreen
